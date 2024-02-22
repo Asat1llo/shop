b4a0ba627a2064474d36d44a00266d6326ed3f61
Asat1lo <asatilloiminjonov57@gmail.com>
2024-02-22 11:04:38 +0500

    push changed

--- src/pages/Bin/index.jsx
+++ src/pages/Bin/index.jsx
@@ -6,12 +6,26 @@ export default function Bin() {
   return (
       <div className={classes.bin}>
       <div className="container">
-        <div className={classes.binWrraper}>
+      <div className={classes.binWrraper}>
             <label  className={classes.binParagref}>Bin</label>
+            <div className={classes.binContainerItems}>
             <div className={classes.binContainer}>
                 <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
             </div>
-        </div>
+            <div className={classes.binContainerTotalPrice}>
+          <div className={classes.binWrraperTotalPrice}>
+            <div className={classes.binContainerTotalValue}>
+              <span className={classes.binTotal}>TOTAL</span>
+              <span className={classes.binTotalValue}>$ 2 927</span>
+            </div>
+            <button className={classes.binTotalBtn}>Next</button>
+          </div>
+            </div>
+            </div>
+       </div>
       </div>
       </div>
   )
--- src/pages/binCard/index.jsx
+++ src/pages/binCard/index.jsx
@@ -1,13 +1,13 @@
 import classes from './binCard.module.css'
 
-import { apple3 } from './../../assets/assets.jsx'
+import { apple3, delet, minus,plus } from './../../assets/assets.jsx'
 
 
 export default function binCard() {
   return (
     <div className={classes.cardWrraper}>
         <div className={classes.cardContainerDelete}>
-            <img src='' alt="delete" />
+            <img src={delet} alt="delete" />
         </div>
         <div className={classes.cardContainerImage}>
             <img className={classes.binImage} src={apple3} alt="img" />
@@ -18,9 +18,9 @@ export default function binCard() {
         </div>
          <div className={classes.cardWrraperCounter}>
           <div className={classes.cardContainerCounter}>
-           <span className={classes.cardPlus}>+</span>
+           <img src={minus} alt='minus' className={classes.cardPlus}/>
            <span className={classes.cardCounterValue}>0</span>
-           <span className={classes.cardPlus}>-</span>     
+           <img src={plus} alt='plus' className={classes.cardPlus}/>     
          </div> 
           <span className={classes.cardTotalPrice}>2 927$</span>
         </div>   
